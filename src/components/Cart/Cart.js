6bbd7e36de272da954d8f8f26085588fc419bc14
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-01-21 03:06:36 +0530

    Add. A4-T7-Creating & Using Cart Context - With Solution

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
 import "./Cart.css";
 
-const Cart = () => {
-  const cartItems = [{ id: "c1", name: "Sushi", quantity: 1, price: 12.99 }];
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const cartItems = cartCtx.items;
 
   return (
-    <Modal>
+    <Modal onBackdropClick={props.onCloseCart}>
       <ul className="cart-items">
         {cartItems.map((item) => {
           return <li key={item.id}>{item.name}</li>;
@@ -17,7 +20,9 @@ const Cart = () => {
         <span>35.62</span>
       </div>
       <div className="actions">
-        <button className="close-btn">Close</button>
+        <button className="close-btn" onClick={props.onCloseCart}>
+          Close
+        </button>
         <button className="order-btn">Order</button>
       </div>
     </Modal>
